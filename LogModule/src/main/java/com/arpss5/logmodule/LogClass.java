34c0ec2f1b8aefc746b1f0df561ee231c90dc8b9
eRohit <eRohit@MacBook-Pro.local>
2020-09-12 11:48:07 +0530

    Initial commit

---
+++ LogModule/src/main/java/com/arpss5/logmodule/LogClass.java
@@ -0,0 +1,9 @@
+package com.arpss5.logmodule;
+
+public class LogClass {
+
+    public void printLog(String message)
+    {
+        System.out.print(message);
+    }
+}
---
+++ app/src/main/java/com/arpss5/utilapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.arpss5.utilapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
